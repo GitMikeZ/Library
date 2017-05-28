94924711bc8c26ac5944e3f49a37878627097fc5
Mike <GitMikeZ@users.noreply.github.com>
2017-05-28 15:55:11 -0700

    Format

--- Python/BST.py
+++ Python/BST.py
@@ -1,41 +1,41 @@
 class Node(object):
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+	def __init__(self, value):
+		self.value = value
+		self.left = None
+		self.right = None
 
 class BST(object):
-  def __init__(self, root):
-    self.root = Node(root)
+	def __init__(self, root):
+		self.root = Node(root)
 
-  def insert(self, new_val):
-    self.insert_helper(self.root, new_val)
+	def insert(self, new_val):
+		self.insert_helper(self.root, new_val)
 
-  def insert_helper(self, current, new_val):
-    if current.value < new_val:
-      if current.right:
-        self.insert_helper(current.right, new_val)
-      else:
-        current.right = Node(new_val)
-    else:
-      if current.left:
-        self.insert_helper(current.left, new_val)
-      else:
-        current.left = Node(new_val)
+	def insert_helper(self, current, new_val):
+		if current.value < new_val:
+			if current.right:
+				self.insert_helper(current.right, new_val)
+			else:
+				current.right = Node(new_val)
+		else:
+			if current.left:
+				self.insert_helper(current.left, new_val)
+			else:
+				current.left = Node(new_val)
 
-  def search(self, val):
-    self.search_helper(self.root, val)
+	def search(self, val):
+		self.search_helper(self.root, val)
 
-  def search_helper(self, current, val):
-    if current:
-      if current.value == val:
-        return True
-      elif current.value < val:
-        self.search_helper(current.right, val)
-      else:
-        self.search_helper(current.left, val)
+	def search_helper(self, current, val):
+		if current:
+			if current.value == val:
+				return True
+			elif current.value < val:
+				self.search_helper(current.right, val)
+			else:
+				self.search_helper(current.left, val)
 
-    return False
+		return False
 
 	def preOrder(self, current):
 		if current:
@@ -43,11 +43,11 @@ class BST(object):
 			preOrder(current.left)
 			preOrder(current.right)
 
-  def inOrder(self, current):
-    if current:
-      self.inOrder(current.left)
-      print(current.value, end=" ")
-      self.inOrder(current.right)
+	def inOrder(self, current):
+		if current:
+			self.inOrder(current.left)
+			print(current.value, end=" ")
+			self.inOrder(current.right)
 
 	def postOrder(self, current):
 		if current:
